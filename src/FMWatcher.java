d37e002d5ac11524dcc3af59797cf4a0ae233150
Francesco Marzolo <checco.marzolo@gmail.com>
2018-06-26 22:01:41 +0200

    0.4.1

--- src/FMWatcher.java
+++ src/FMWatcher.java
@@ -22,7 +22,7 @@ public class FMWatcher extends JAddinThread {
 	WatchService myWatcher ;
 	FMDirChangeEventReceiver dirChangeEventReceiver;
 	Thread thread4DirChangeEventReceiver ;
-	static final String FMWATCHERVERSION="0.4";
+	static final String FMWATCHERVERSION="0.4.1";
 	private int varsMax=10;
 
 	private FMBase base=null;
